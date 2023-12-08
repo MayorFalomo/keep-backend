2608aac64175935bf39e478f14bc49160451dd94
mayor falomo <mayorfalomo@gmail.com>
2023-12-08 05:48:45 +0100

    removed comments in index.js

--- index.js
+++ index.js
@@ -1,54 +1,3 @@
-// import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import cors from "cors";
-
-// const noteRoutes = require("./routes/notes");
-// const pinnedRoutes = require("./routes/pinned");
-// const archivedRoutes = require("./routes/archive");
-// const userRoutes = require("./routes/users");
-
-// // For the .env file
-// dotenv.config({ path: "./vars/.env" });
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-// app.use(express.json());
-// const corsConfig = {
-//   origin: "",
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-// };
-// app.use(cors(corsConfig));
-// app.options("", cors(corsConfig));
-
-// app.get("/", () => {
-//   res.send("Welcome to Express & TypeScript Server");
-// });
-
-// mongoose
-//   .connect(
-//     `mongodb+srv://KaladinReborn:mayowa@cluster0.g848ftj.mongodb.net/keep?retryWrites=true&w=majority`,
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
-//   .then(() => {
-//     console.log("Connected to MongoDB");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// app.use("/api/notes", noteRoutes);
-// app.use("/api/notes", pinnedRoutes);
-// app.use("/api/notes", archivedRoutes);
-// app.use("/api/users", userRoutes);
-
-// app.listen(port, () => {
-//   console.log(`Server is firing at http://localhost:${port}`);
-// });
-
 const express = require("express");
 const app = express();
 const dotEnv = require("dotenv");
@@ -72,6 +21,10 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.get("/", () => {
+  res.send("Welcome to Express & TypeScript Server");
+});
 
 app.use("/api/notes", noteRoutes);
 app.use("/api/notes", pinnedRoutes);
